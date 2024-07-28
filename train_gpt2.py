b1d00790b1df71b51255f674c6ca59412b52df94
Javad Nouri <jvdnouri@gmail.com>
2024-07-28 22:01:59 +0000

    doing distributed trainign

--- train_gpt2.py
+++ train_gpt2.py
@@ -11,16 +11,37 @@ import time
 import wandb
 import math
 import inspect
-
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
 
 logging.getLogger().setLevel(logging.INFO)
+import os
+
+IS_DDP_RUN = "RANK" in os.environ
+if IS_DDP_RUN:
+    dist.init_process_group(backend="nccl")
+    assert torch.cuda.is_available(), "CUDA must be available for ddp run"
+RANK = int(os.environ.get("RANK", 0))
+WORLD_SIZE = int(os.environ.get("WORLD_SIZE", 1))
+LOCAL_RANK = int(os.environ.get("LOCAL_RANK", -1))
+IS_MASTER = RANK == 0
 
 
 def log(*args, **kwargs):
-    logging.info(*args, **kwargs)
+    if IS_MASTER:
+        logging.info(*args, **kwargs)
+
+
+def log_wandb(*args, **kwargs):
+    if IS_MASTER:
+        wandb.log(*args, **kwargs)
 
 
 def detect_device():
+    if IS_DDP_RUN:
+        device = f"cuda:{RANK}"
+        torch.cuda.set_device(device)
+        return device
     device = "cpu"
     if torch.cuda.is_available():
         device = "cuda"
@@ -47,6 +68,8 @@ class DataLoader:
         block_size: int,
         model_name: str = "gpt2",
         device: str | None = None,
+        rank: int = 0,
+        world_size: int = 1,
     ) -> None:
         with open(file_name, "r") as f:
             text = f.read()
@@ -58,7 +81,9 @@ class DataLoader:
         self.n_tokens = self.data.size(0)
         self.batch_size = batch_size
         self.block_size = block_size
-        self.current_pos = 0
+        self.rank = rank
+        self.world_size = world_size
+        self.current_pos = self.rank * self.batch_size * self.block_size
 
     def next_batch(self):
         B = self.batch_size
@@ -66,9 +91,9 @@ class DataLoader:
         buf = self.data[self.current_pos : B * T + self.current_pos + 1]
         x = buf[:-1].view(-1, T)
         y = buf[1:].view(-1, T)
-        self.current_pos += B * T
-        if self.current_pos + (B * T + 1) > self.data.size(0):
-            self.current_pos = 0
+        self.current_pos += B * T * self.world_size
+        if self.current_pos + (B * T * self.world_size + 1) > self.data.size(0):
+            self.current_pos = self.rank * self.batch_size * self.block_size
         return x, y
 
 
@@ -125,8 +150,7 @@ def get_optimizer(
     return optimizer
 
 
-if __name__ == "__main__":
-    USE_WANDB = True
+def train(USE_WANDB=False):
     train_config = GPTTrainConfig()
     optimizer_config = OptimizerConfig()
     model_config = GPTConfig(vocab_size=50304)
@@ -135,34 +159,49 @@ if __name__ == "__main__":
         train_config=train_config,
         optimizer_config=optimizer_config,
     )
-    if USE_WANDB:
-        wandb.init(project="gpt2", name="debug GPT Train 10 epochs", config=config)
+    if USE_WANDB and IS_MASTER:
+        wandb.init(
+            project="gpt2", name="GPT Train with gradient accumulation", config=config
+        )
     device = detect_device()
     if train_config.seed is not None:
         torch.manual_seed(train_config.seed)
         if torch.cuda.is_available():
             torch.cuda.manual_seed(train_config.seed)
 
-    log(f"Using {device=}")
+    logging.info(f"Using {device=}")
 
     torch.set_float32_matmul_precision(train_config.float32_matmul_precision)
 
     tokenizer = tiktoken.get_encoding("gpt2")
     # gpt = GPT.from_pretrained("gpt2")
     gpt = GPT(model_config)
+    gpt.to(device)
     log("compiling torch model...")
     gpt = torch.compile(gpt)
     log(f"{gpt.config=}")
 
     micro_batch_size = train_config.micro_batch_size
+    data_loader = DataLoader(
+        file_name="input.txt",
+        model_name="gpt2",
+        batch_size=micro_batch_size,
+        block_size=gpt.config.block_size,
+        rank=RANK,
+        world_size=WORLD_SIZE,
+    )
+    if IS_DDP_RUN:
+        gpt = DDP(gpt, device_ids=[LOCAL_RANK])
+
     assert (
         train_config.tokens_per_batch
-        % (model_config.block_size * train_config.micro_batch_size)
+        % (model_config.block_size * train_config.micro_batch_size * WORLD_SIZE)
         == 0
     )
     gradient_accum_batch_size = train_config.tokens_per_batch // (
-        model_config.block_size * train_config.micro_batch_size
+        model_config.block_size * train_config.micro_batch_size * WORLD_SIZE
     )
+    log(f"will use {gradient_accum_batch_size} gradient accumulation steps")
     log(f"found {count_params(gpt)} parameters")
     log(f"parameters size ~ {get_memory_size(gpt) / 1024 / 1024:.2f} MB")
     gpt.to(device)
@@ -175,19 +214,25 @@ if __name__ == "__main__":
     )
     log(f"Will train for {n_steps} steps")
     optimizer = get_optimizer(optimizer_config, gpt, device=device)
+    device_type = "cuda" if "cuda" in device else device
     for step in range(n_steps):
         time0 = time.time()
         optimizer.zero_grad()
         total_loss = 0.0
-        for _ in range(gradient_accum_batch_size):
+        for microstep in range(gradient_accum_batch_size):
             x, y = data_loader.next_batch()
             x = x.to(device)
             y = y.to(device)
-            with torch.autocast(device_type=device, dtype=torch.bfloat16):
+            with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
                 logits, loss = gpt(x, y)
             loss /= gradient_accum_batch_size
             total_loss += loss.detach()
+            if IS_DDP_RUN:
+                gpt.require_backward_grad_sync = (
+                    microstep == gradient_accum_batch_size - 1
+                )
             loss.backward()
+        dist.all_reduce(total_loss, op=dist.ReduceOp.AVG)
 
         gradient_norm = torch.nn.utils.clip_grad_norm_(
             gpt.parameters(), max_norm=optimizer_config.clip_grad_max_norm
@@ -199,6 +244,7 @@ if __name__ == "__main__":
         torch.cuda.synchronize()
         time1 = time.time()
         total_time = time1 - time0
+        assert  train_config.tokens_per_batch == train_config.micro_batch_size * block_size * WORLD_SIZE * gradient_accum_batch_size
         throughput = train_config.tokens_per_batch / total_time
         log_payload = dict(
             lr=lr, loss=total_loss, throughput=throughput, gradient_norm=gradient_norm
@@ -213,7 +259,7 @@ if __name__ == "__main__":
             )
         )
         if USE_WANDB:
-            wandb.log(log_payload)
+            log_wandb(log_payload)
 
     # seed_text = "Hello, I am a language model,"
     # gpt.eval()
@@ -222,3 +268,9 @@ if __name__ == "__main__":
     # for sentence in my_generator.generate(seed_text, 100, 3):
     #     print(sentence)
     #     print("_" * 40)
+
+
+if __name__ == "__main__":
+    train(USE_WANDB=True)
+    if IS_DDP_RUN:
+        dist.destroy_process_group()
