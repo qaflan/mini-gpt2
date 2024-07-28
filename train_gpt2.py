98bb512402fd6b558d55990d629ed5501a921b30
Javad Nouri <jvdnouri@gmail.com>
2024-07-28 19:45:41 +0000

    making wandb controllable

--- train_gpt2.py
+++ train_gpt2.py
@@ -64,10 +64,11 @@ class DataLoader:
 
 
 if __name__ == "__main__":
+    USE_WANDB = False
     train_config = GPTTrainConfig()
     model_config = GPTConfig(vocab_size=50304)
-    config = Config(model_config=model_config, train_config=train_config)
-    wandb.init(project="gpt2", name="GPT Train 10 epochs", config=config)
+    if USE_WANDB:
+        wandb.init(project="gpt2", name="debug GPT Train 10 epochs", config=config)
     device = detect_device()
     if train_config.seed is not None:
         torch.manual_seed(train_config.seed)
@@ -113,10 +114,8 @@ if __name__ == "__main__":
         time1 = time.time()
         total_time = time1 - time0
         throughput = (train_config.batch_size * gpt.config.block_size) / total_time
-        logging.info(
-            f"step {i:3d}, loss: {loss.item():4f} took {total_time*1000:.2f} milliseconds @ {throughput:.2f} tokens/sec"
-        )
-        wandb.log(dict(loss=loss.item(), throughput=throughput))
+        if USE_WANDB:
+            wandb.log(dict(loss=loss.item(), throughput=throughput))
 
     # seed_text = "Hello, I am a language model,"
     # gpt.eval()
