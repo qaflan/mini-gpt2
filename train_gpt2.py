82c48d242e1a6330cd5902e5f8b6bf3170154dc8
Javad Nouri <jvdnouri@gmail.com>
2024-07-31 21:59:35 +0000

    removed unnecessary code

--- train_gpt2.py
+++ train_gpt2.py
@@ -36,15 +36,6 @@ def set_logging_params()->None:
 if IS_DDP_RUN:
     dist.init_process_group(backend="nccl")
     assert torch.cuda.is_available(), "CUDA must be available for ddp run"
-RANK = int(os.environ.get("RANK", 0))
-WORLD_SIZE = int(os.environ.get("WORLD_SIZE", 1))
-LOCAL_RANK = int(os.environ.get("LOCAL_RANK", -1))
-IS_MASTER = RANK == 0
-
-
-def log(*args, **kwargs):
-    if IS_MASTER:
-        logging.info(*args, **kwargs)
 
 
 def log_wandb(*args, **kwargs):
