dae42f827f2102b421c2a2509af11183d252f89f
Javad Nouri <jvdnouri@gmail.com>
2024-08-07 04:32:47 +0000

    added detect_device

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import os
 import logging
 
+import torch
+
 IS_DDP_RUN = "RANK" in os.environ
 RANK = int(os.environ.get("RANK", 0))
 WORLD_SIZE = int(os.environ.get("WORLD_SIZE", 1))
@@ -11,3 +13,16 @@ IS_MASTER = RANK == 0
 def log(*args, **kwargs):
     if IS_MASTER:
         logging.info(*args, **kwargs)
+
+
+def detect_device():
+    if IS_DDP_RUN:
+        device = f"cuda:{RANK}"
+        torch.cuda.set_device(device)
+        return device
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+        device = "mps"
+    return device
